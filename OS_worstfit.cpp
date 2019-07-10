88cd7fdc97ec0482fe212b48a8f9221dd20059e3
Abdullah Al Numan <34994789+Numan2962@users.noreply.github.com>
2019-07-10 19:26:37 +0600

    Add files via upload

---
+++ OS_worstfit.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fit[100000];
+int main()
+{
+    int i,j,p[100],b[100],bn,pn;
+    cin>>bn;
+    for(i=0;i<bn;i++)cin>>b[i];
+    cin>>pn;
+    for(i=0;i<pn;i++)cin>>p[i];
+    for(i=0;i<pn;i++)
+        for(j=bn-1;j>=0;j--)
+            if((p[i]<=b[j]))
+                if(fit[b[j]]==0)
+                {
+                   fit[b[j]]=p[i];
+                   break;
+                }
+    cout<<endl;
+    for(i=0;i<bn;i++)
+        cout<<fit[b[i]]<<"  \t"<<b[i]<<"  \t"<<b[i]-fit[b[i]]<<endl;
+    return 0;
+}
